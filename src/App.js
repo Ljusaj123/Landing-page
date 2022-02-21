6a5a6e609057ec056f2c65da35a82d28e598275c
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-21 16:38:53 +0100

    whole structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-
 function App() {
   return (
     <div className="landing-page">
-      
+      <div className="landing-page-container">
+        <header className="header">
+          <div className="header-container">
+            <div className="logo-container">
+              <h1>Logo</h1>
+            </div>
+            <nav className="navbar-menu-container">
+              <li className="navbar-links"><a href="#" className="navbar-items">Home</a></li>
+              <li className="navbar-links"><a href="#" className="navbar-items">About</a></li>
+              <li className="navbar-links"><a href="#" className="navbar-items">Services</a></li>
+              <button className="btn">Learn more</button>
+            </nav>
+          </div>
+        </header>
+        <main className="main">
+          <div className="main-container">
+            <h1>Lorem ipsum</h1>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+              Facilis repellat dignissimos, eaque vel, soluta odio tempore 
+              quisquam vero repudiandae voluptates illo eveniet quia? Explicabo 
+              ullam corporis velit facilis mollitia sunt quis, autem itaque provident 
+              deleniti odio assumenda atque accusantium. Id!</h2>
+              <button className="btn">Learn more</button>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
