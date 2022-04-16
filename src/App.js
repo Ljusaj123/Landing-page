99366d984acf76cbd2ff6851c7f2af80dae25bbf
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-16 23:29:10 +0200

    formatted, added style for mobile version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [navbar, setNavbar] = useState(false);
 
-  const [navbar, setNavbar]=useState(false);
+  const showNavbar = () => {
+    setNavbar((prev) => !prev);
+  };
 
-  const showNavbar = () =>{
-    setNavbar(prev => !prev);
-
-  }
-  
   return (
     <div className="landing-page">
       <div className="landing-page-container">
@@ -17,28 +15,70 @@ function App() {
             <div className="logo-container">
               <h1>Logo</h1>
             </div>
-            <nav className={navbar ? "navbar-menu-container active" : "navbar-menu-container"}>
-              <li className="navbar-links"><a href="#" className="navbar-items">Home</a></li>
-              <li className="navbar-links"><a href="#" className="navbar-items">About</a></li>
-              <li className="navbar-links"><a href="#" className="navbar-items">Services</a></li>
-              <li className="navbar-links"><a href="#" className="navbar-items">Contact</a></li>
+            <nav
+              className={
+                navbar
+                  ? "navbar-menu-container active"
+                  : "navbar-menu-container"
+              }
+            >
+              <li className="navbar-links">
+                <a href="/" className="navbar-items">
+                  Home
+                </a>
+              </li>
+              <li className="navbar-links">
+                <a href="/" className="navbar-items">
+                  About
+                </a>
+              </li>
+              <li className="navbar-links">
+                <a href="/" className="navbar-items">
+                  Services
+                </a>
+              </li>
+              <li className="navbar-links">
+                <a href="/" className="navbar-items">
+                  Contact
+                </a>
+              </li>
             </nav>
             <div className="navbar-hamburger" onClick={showNavbar}>
-					    <li className={navbar ? "navbar-hamburger-line open" : "navbar-hamburger-line" }></li>
-					    <li className={navbar ? "navbar-hamburger-line open" : "navbar-hamburger-line" }></li>
-					    <li className={navbar ? "navbar-hamburger-line open" : "navbar-hamburger-line" }></li>
-				    </div>
+              <li
+                className={
+                  navbar
+                    ? "navbar-hamburger-line open"
+                    : "navbar-hamburger-line"
+                }
+              ></li>
+              <li
+                className={
+                  navbar
+                    ? "navbar-hamburger-line open"
+                    : "navbar-hamburger-line"
+                }
+              ></li>
+              <li
+                className={
+                  navbar
+                    ? "navbar-hamburger-line open"
+                    : "navbar-hamburger-line"
+                }
+              ></li>
+            </div>
           </div>
         </header>
         <main className="main">
           <div className="main-container">
             <h1>Lorem ipsum</h1>
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-              Facilis repellat dignissimos, eaque vel, soluta odio tempore 
-              quisquam vero repudiandae voluptates illo eveniet quia? Explicabo 
-              ullam corporis velit facilis mollitia sunt quis, autem itaque provident 
-              deleniti odio assumenda atque accusantium. Id!</h2>
-              <button className="btn">Learn more</button>
+            <h2>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+              repellat dignissimos, eaque vel, soluta odio tempore quisquam vero
+              repudiandae voluptates illo eveniet quia? Explicabo ullam corporis
+              velit facilis mollitia sunt quis, autem itaque provident deleniti
+              odio assumenda atque accusantium. Id!
+            </h2>
+            <button className="btn">Learn more</button>
           </div>
         </main>
       </div>
